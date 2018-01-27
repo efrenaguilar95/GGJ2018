0eae90ebcaf5b27dfae55d9ae3df5a04f6c887a1
Suds <ace.lowder@gmail.com>
2018-01-26 23:56:27 -0800

    Added Custom Character/Camera Scripts
    Created copies of the asset scripts for us to easily edit to fit our needs in the project

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof (CharacterController))]
+public class InputManager : MonoBehaviour
+{
+	private CharacterController m_Character;
+	private bool m_Jump;
+
+
+	private void Awake()
+	{
+		m_Character = GetComponent<CharacterController>();
+	}
+
+
+	private void Update()
+	{
+		if (!m_Jump)
+		{
+			// Read the jump input in Update so button presses aren't missed.
+			m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+		}
+	}
+
+
+	private void FixedUpdate()
+	{
+		// Read the inputs.
+		bool crouch = Input.GetKey(KeyCode.LeftControl);
+		float h = CrossPlatformInputManager.GetAxis("Horizontal");
+		// Pass all parameters to the character control script.
+		m_Character.Move(h, crouch, m_Jump);
+		m_Jump = false;
+	}
+}
