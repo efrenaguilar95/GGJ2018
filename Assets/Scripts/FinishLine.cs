319624afb7f444c71d00a456e31bd43c02da54cd
KupoGrounds <yugiohaddictben@gmail.com>
2018-01-27 13:17:18 -0800

    StarCounter script and near-complete FinishLine script
    Star Counter script is done. FinishLine script is technically done. Needs the ResultsScreen script to be completed.

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Needed for text
+
+public class FinishLine : MonoBehaviour {
+    public Text victoryText;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject hitObj = collider.gameObject;
+        if (hitObj.tag == "Player")
+        {
+            //transform.parent.gameObject.AddComponent<ResultsScreen>;
+            victoryText.text = "Hurray!";
+        }
+    }
+}
---
+++ Assets/Scripts/StarCounter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarCounter : MonoBehaviour {
+
+    public float starGoal1;
+    public float starGoal2;
+    public float starGoal3;
+
+    public float GetStarGoal1()
+    {
+        return starGoal1;
+    }
+
+    public float GetStarGoal2()
+    {
+        return starGoal2;
+    }
+		
+	public float GetStarGoal3()
+    {
+        return starGoal3;
+    }
+}
