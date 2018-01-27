942718d564b53620404d3b52521394dfdf929520
Suds <ace.lowder@gmail.com>
2018-01-27 12:13:04 -0800

    Added Basic Killzone
    Restarts the scene when it collides with the player. At the moment it restarts the current scene.

---
+++ Assets/Scripts/KillZoneController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillZoneController : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+}
