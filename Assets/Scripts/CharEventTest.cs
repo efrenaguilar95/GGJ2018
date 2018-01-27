55eb8664943d4d554961f79ddaf3fa121fab0ac0
Anthony Ha <haa4@uci.edu>
2018-01-27 10:07:17 -0800

    Refined dialogue script; CharEventTest script is an example of how to trigger dialogue from a specific character

---
+++ Assets/Scripts/CharEventTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharEventTest : MonoBehaviour {
+	public GameObject dialogManager;
+	public TextAsset textFile;
+	// Use this for initialization
+	void Start () {
+		dialogManager = gameObject.transform.Find ("Canvas/DialogueManager").gameObject;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Semicolon))
+			dialogManager.GetComponent<CharacterDialogue> ().ToggleBox (textFile.text);
+		
+	}
+}
