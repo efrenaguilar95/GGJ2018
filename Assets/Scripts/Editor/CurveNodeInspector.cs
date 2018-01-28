113c2714afcefd9897773480c0400db57a134ab5
andresg6 <andresg6@uci.edu>
2018-01-27 20:10:07 -0800

    BezierSpline Inital Editor Implementation

--- Assets/Scripts/Editor/CurveNodeInspector.cs
+++ Assets/Scripts/Editor/CurveNodeInspector.cs
@@ -4,36 +4,129 @@ using UnityEditor;
 using UnityEngine;
 
 #if UNITY_EDITOR
-
 //Made w/ help of tutorial http://catlikecoding.com/unity/tutorials/curves-and-splines/
 [CustomEditor(typeof(CurveNode))]
-public class CurveNodeInspector : Editor {
+public class CurveNodeInspector : Editor
+{
+    public enum SelectedCurveUI
+    {
+        outTangent,
+        inTangent,
+        unknown
+    }
+
+    private CurveNode node;
+    private BezierSpline parentSpline;
+    private Transform handleTransform;
+    private Quaternion handleRotation;
+
+    private SelectedCurveUI selectedUI;
+
+    private const float handleSize = 0.04f;
+    private const float pickSize = 0.06f;
+    private const int lineRenderSteps = 20;
+
+
     private void OnSceneGUI()
     {
-        //Data
-        CurveNode node = target as CurveNode;
-        Transform handleTransform = node.transform;
-        Quaternion handleRotation = (Tools.pivotRotation == PivotRotation.Local ? 
-            node.transform.rotation : Quaternion.identity);
-        Vector2 p0 = handleTransform.TransformPoint(node.inTangent); //inTangent
-        Vector2 p1 = handleTransform.TransformPoint(node.outTangent); //outTangent
-        //Rendering
-        Handles.color = Color.white;
-        Handles.DrawLine(p0, node.transform.position);
-        Handles.DrawLine(node.transform.position, p1);
+        DrawGUI(true);
+    }
 
-        //Interactable GUI
-        //Update Event Handling?
-        //AG: This not being a hookable event feels wrong
-        EditorGUI.BeginChangeCheck();
-        Vector2 hp0 = Handles.DoPositionHandle(p0, handleRotation);
-        Vector2 hp1 = Handles.DoPositionHandle(p1, handleRotation);
-        if(EditorGUI.EndChangeCheck())
+    public void DrawGUI(bool renderCurves)
+    {
+        //Data
+        node = target as CurveNode;
+        parentSpline = node.GetComponentInParent<BezierSpline>();
+        handleTransform = node.transform;
+        handleRotation = (Tools.pivotRotation == PivotRotation.Local ?
+            node.transform.rotation : Quaternion.identity);
+
+        //Rendering
+        SetInTangentGizmo();
+        SetOutTangentGizmo();
+
+        if(renderCurves && parentSpline != null)
         {
-            Undo.RecordObject(node, "Moved Tangent");
-            EditorUtility.SetDirty(node);
-            node.inTangent = handleTransform.InverseTransformPoint(hp0);
-            node.outTangent = handleTransform.InverseTransformPoint(hp1);
+            if(node.index == null || node.index == -1)
+            {
+                node.index = parentSpline.GetIndexOfNode(node);
+            }
+            if(node.index != -1)
+            {
+                RenderAdjacentCurves();
+            }
+        }
+    }
+
+    private Vector2 SetInTangentGizmo()
+    {
+        return SetTangentGizmo(ref node.inTangent, SelectedCurveUI.inTangent);
+    }
+
+    private Vector2 SetOutTangentGizmo()
+    {
+       return SetTangentGizmo(ref node.outTangent, SelectedCurveUI.outTangent);
+    }
+
+    private Vector2 SetTangentGizmo(ref Vector2 tangent, SelectedCurveUI requestedUI)
+    {
+        Handles.color = Color.white;
+        Vector2 point = handleTransform.TransformPoint(tangent);
+        if (Handles.Button(point, handleRotation, handleSize, pickSize, Handles.DotHandleCap))
+        {
+            selectedUI = requestedUI;
+        }
+        if(selectedUI == requestedUI)
+        {
+            EditorGUI.BeginChangeCheck();
+            point = Handles.DoPositionHandle(point, handleRotation);
+            if (EditorGUI.EndChangeCheck())
+            {
+                EditorUtility.SetDirty(node);
+                tangent = handleTransform.InverseTransformPoint(point);
+            }
+        }
+
+        Handles.DrawLine(point, node.transform.position);
+        return point;
+    }
+
+    public void RenderAdjacentCurves()
+    {
+        if(node.index != null && parentSpline != null)
+        {
+            //Render connection to previous node
+            if(node.index == 0)
+            {
+                if (parentSpline.connectiveness == BezierSpline.SplineLink.Loop)
+                    RenderCurve(parentSpline.points[parentSpline.points.Count - 1], node);
+            }
+            else
+            {
+                RenderCurve(parentSpline.points[node.index.Value - 1], node);
+            }
+            //Render connection to next node
+            if(node.index == parentSpline.points.Count - 1)
+            {
+                if (parentSpline.connectiveness == BezierSpline.SplineLink.Loop)
+                    RenderCurve(node, parentSpline.points[0]);
+            }
+            else
+            {
+                RenderCurve(node, parentSpline.points[node.index.Value + 1]);
+            }
+        }
+    }
+
+    public static void RenderCurve(CurveNode lhs, CurveNode rhs)
+    {
+        Handles.color = Color.white;
+        Vector2 lineStart = CurveNode.GetInvervalPosition(lhs, rhs, 0f);
+        for (int i = 0; i < lineRenderSteps; i++)
+        {
+            Vector2 lineEnd = CurveNode.GetInvervalPosition(lhs, rhs, (i + 1) / (float)lineRenderSteps);
+            Handles.DrawLine(lineStart, lineEnd);
+            lineStart = lineEnd;
         }
     }
 }
