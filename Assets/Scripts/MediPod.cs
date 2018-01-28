68fc365fde4257e5659f4d8a985326c02cb30e3a
KupoGrounds <yugiohaddictben@gmail.com>
2018-01-27 22:36:04 -0800

    added MediPod script and prefab
    MediPod is the bunker thing. when it collids with a "Human" tagged object, they are destroyed (so the people are saved)

---
+++ Assets/Scripts/MediPod.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediPod : MonoBehaviour {
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        GameObject hitObj = collider.gameObject;
+        if (hitObj.tag == "Human")
+        {
+            Destroy(hitObj);
+        }
+    }
+}
