8da62c4c75b203171056b1c814e73f1a725c2002
KupoGrounds <yugiohaddictben@gmail.com>
2018-01-27 17:41:11 -0800

    EnemySpike script, GameOverManager script complete
    Spike and GameOver sripts are done. Spike interacts with GameOverManager by sending the failState of the game over, which will tell the GameOverManager to restart the game or keep on going

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour {
+
+    public float restartDelay = 5f;
+    public bool failState = false;
+    public EnemySpike spike;
+
+    private Animator anim;
+    private float restartTimer;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+
+        if(spike.GetFailState() == true)
+        {
+            anim.SetTrigger("GameOver");
+            restartTimer += Time.deltaTime;
+
+            if(restartTimer >= restartDelay)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+}
