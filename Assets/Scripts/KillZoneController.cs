b0b129e871c57a6298ab49097fbcfeabebdb463f
KupoGrounds <yugiohaddictben@gmail.com>
2018-01-28 13:47:36 -0800

    Changed killzonecontroller
    KillZoneController now uses the GameOverManager to restart. Make sure the Canvas prefab is in the scene, then attach the Canvas's GameOverManager component to the KillZone

--- Assets/Scripts/KillZoneController.cs
+++ Assets/Scripts/KillZoneController.cs
@@ -5,12 +5,13 @@ using UnityEngine.SceneManagement;
 public class KillZoneController : MonoBehaviour
 {
 	public GameObject m_Player;
+    public GameOverManager gameOver;
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "Player")
 		{
-			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+            gameOver.SetFailState(true);
 		}
 		else if (other.tag == "Human")
 		{
