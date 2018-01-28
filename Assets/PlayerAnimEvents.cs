f62d51bdd209f6560018d28241e2098c4bb211c8
Efren Aguilar <efrenaguilar95@hotmail.com>
2018-01-27 22:12:20 -0800

    Test Footstep Sounds

---
+++ Assets/PlayerAnimEvents.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimEvents : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public List<AudioClip> footstepSounds;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = this.GetComponent<AudioSource>();
+	}
+
+
+    void PlayerFootstep()
+    {
+        audioSource.PlayOneShot(footstepSounds[Random.Range(0, footstepSounds.Count)]);
+    }
+}
