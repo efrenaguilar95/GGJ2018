4f7ba4b99d341f1386a65f84d1439874b8be77ce
Efren Aguilar <efrenaguilar95@hotmail.com>
2018-01-28 16:03:29 -0800

    Quick fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -187,6 +187,8 @@ public class PlayerController : MonoBehaviour
 
     private void AttemptTravel()
     {
+        if (IsCarrying())
+            return;
         // test is on start node
         switch (m_Electric)
         {
