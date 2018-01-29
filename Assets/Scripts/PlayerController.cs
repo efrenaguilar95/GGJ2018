ad0d6610ac8c6111ae47859289ca35e65a23075e
Efren Aguilar <efrenaguilar95@hotmail.com>
2018-01-28 16:05:25 -0800

    Quick Fix 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -214,6 +214,7 @@ public class PlayerController : MonoBehaviour
                         m_Electric = true;
                         Debug.Log("Attempted to Travel");
                         audioSource.PlayOneShot(electrcity);
+                        ChangeHappiness(-5);
                     }
                     else
                     {
